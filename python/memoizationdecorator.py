14ce3f32107d28beec24a24a74384deb7f8fe6a1
Andreas Mayer <andisspam@gmail.com>
2017-03-29 01:44:43 +0200

    added python function

---
+++ python/memoizationdecorator.py
@@ -0,0 +1,22 @@
+
+class memoized(object):
+    """Decorator. Caches a function's return value each time it is called.
+    If called later with the same arguments, the cached value is returned
+    (not reevaluated).
+    """
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+
+    def __call__(self, *args):
+        # if args is not Hashable we can't cache
+        # easier to ask for forgiveness than permission
+        try:
+            if args in self.cache:
+                return self.cache[args]
+            else:
+                value = self.func(*args)
+                self.cache[args] = value
+                return value
+        except TypeError:
+            return self.func(*args)
